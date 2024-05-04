9b33a33202af724e2e13b4fbaa807ecb1ebfd98e
Yoni Bensoussan <bns@air-de-yoni.home>
2024-05-04 05:02:06 +0200

    v3.2

--- src/components/ContactLinks.jsx
+++ src/components/ContactLinks.jsx
@@ -2,27 +2,30 @@
 import { Github, Linkedin, Mail } from 'lucide-react';
 import confetti from 'canvas-confetti';
 
-const handleConfettiAndRedirect = (event, redirectUrl) => {
+const handleConfetti = (event) => {
     event.preventDefault();
     confetti({
         particleCount: 150,
         spread: 70,
         origin: { y: 0.6 }
     });
+    const redirectUrl = event.currentTarget.getAttribute("data-url");
 
-    setTimeout(() => { window.open(redirectUrl, '_blank'); }, 1500);
+    setTimeout(() => {
+        window.location.href = redirectUrl;
+    }, 1000);
 };
 
 const ContactLinks = ({ className }) => {
     return (
         <div className={`inline-flex p-1 items-center space-x-3 mt-2 justify-center rounded-lg border bg-card text-card-foreground shadow-sm ${className}`}>
-            <button className="hover:text-muted-foreground transition-colors duration-300" onClick={(e) => handleConfettiAndRedirect(e, "mailto:contact@yonibns.com")}>
+            <button className="hover:text-muted-foreground transition-colors duration-300" onClick={handleConfetti} data-url="mailto:contact@yonibns.com">
                 <Mail size={17} />
             </button>
-            <button className="hover:text-muted-foreground transition-colors duration-300" onClick={(e) => handleConfettiAndRedirect(e, "https://www.linkedin.com/in/yonibns")}>
+            <button className="hover:text-muted-foreground transition-colors duration-300" onClick={handleConfetti} data-url="https://www.linkedin.com/in/yonibns">
                 <Linkedin size={17} />
             </button>
-            <button className="hover:text-muted-foreground transition-colors duration-300" onClick={(e) => handleConfettiAndRedirect(e, "https://github.com/yobns")}>
+            <button className="hover:text-muted-foreground transition-colors duration-300" onClick={handleConfetti} data-url="https://github.com/yobns">
                 <Github size={17} />
             </button>
         </div>
