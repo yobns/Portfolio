96b8c2ad287bd8ef539ed62b69b24e848141a127
Yoni Bensoussan <yonibnss@gmail.com>
2024-10-17 17:44:41 +0200

    42 added

--- src/components/Status/Data.jsx
+++ src/components/Status/Data.jsx
@@ -13,8 +13,8 @@ export const WORK = [
 ];
 
 export const EDUCATION = [
+    { image: "./logo/42.jpg", title: "42", role: "IT School", date: "2024 - Now", url: "http://www.42.fr" },
     { image: "./logo/ITC.jpg", title: "ITC", role: "Full Stack Program", date: "2023 - 2024", url: "https://www.itc.tech" },
-    { image: "./logo/42.jpg", title: "42", role: "IT School", date: "2022 - 2023", url: "http://www.42.fr" },
-    { image: "./logo/epitech.jpg", title: "Epitech", role: "IT School", date: "2021 - 2022", url: "http://www.epitech.eu" },
+    // { image: "./logo/epitech.jpg", title: "Epitech", role: "IT School", date: "2021 - 2022", url: "http://www.epitech.eu" },
     { image: "./logo/ibs.png", title: "IBS", role: "High School", date: "2019 - 2020", url: "https://www.linkedin.com/company/15215918/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3Bhsw%2BnEUcRxma4fT2raxI2w%3D%3D" },
 ];
