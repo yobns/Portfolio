b68900d33997080927eea47913af5b8340541c3f
Yoni Bensoussan <yonibnss@gmail.com>
2024-10-19 21:01:19 +0200

    v4.1

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,46 +1,55 @@
 "use client";
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import { Share_Tech_Mono, Homemade_Apple } from 'next/font/google'
+import { Share_Tech_Mono, Homemade_Apple } from 'next/font/google';
 
 const techInit = Share_Tech_Mono({
     subsets: ['latin'],
     weight: ['400'],
     variable: '--font-tech',
-})
+});
 
 const appleInit = Homemade_Apple({
     subsets: ['latin'],
     weight: ['400'],
     variable: '--font-homemade-apple',
-})
+});
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
+    const [darkMode, setDarkMode] = useState(false);
     const [isMounted, setIsMounted] = useState(false);
-    let savedDarkMode = false;
-    if (typeof window !== "undefined" && isMounted) {
-        const saved = localStorage.getItem('darkMode');
-        savedDarkMode = saved !== null ? JSON.parse(saved) : window.matchMedia('(prefers-color-scheme: dark)').matches;
-    }
-    const [darkMode, setDarkMode] = useState(savedDarkMode);
 
     useEffect(() => {
-        setIsMounted(true);
-        if (typeof window !== "undefined") {
-            localStorage.setItem('darkMode', JSON.stringify(darkMode));
+        if (typeof window !== 'undefined') {
+            const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+            setDarkMode(mediaQuery.matches);
+
+            const handleChange = (e) => setDarkMode(e.matches);
+            mediaQuery.addEventListener('change', handleChange);
+
+            return () => mediaQuery.removeEventListener('change', handleChange);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (isMounted) {
+            document.body.classList.toggle('dark', darkMode);
+            document.body.classList.toggle('light', !darkMode);
         }
     }, [darkMode]);
 
-    const toggleDarkMode = () => {
-        setDarkMode(prevMode => !prevMode);
-    };
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-    const bodyClass = darkMode ? 'dark' : 'light';
+    const toggleDarkMode = () => {
+        setDarkMode((prevMode) => !prevMode);
+    };
 
     return (
         <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
-            <body className={`${isMounted ? bodyClass : ''} ${techInit.variable} ${appleInit.variable}`}>
+            <body className={`${techInit.variable} ${appleInit.variable}`}>
                 {children}
             </body>
         </ThemeContext.Provider>
